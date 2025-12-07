44dfe9db8d36cbd6e1856c309271275e2dc59a02
xunyi <xunyi15833234696@163.com>
2025-12-07 19:24:30 +0800

    Add files via upload

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -54,8 +54,10 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
+        Chevron: ({ orientation, ...props }) => {
+          const Icon = orientation === "right" ? ChevronRight : ChevronLeft;
+          return <Icon className="h-4 w-4" />;
+        },
       }}
       {...props}
     />
